d13e91635e54c086cb4bd44ea38bd20b4f05da7d
syed-basit-ali <basit.shah94@gmail.com>
2023-04-13 13:35:26 +0500

    Feature: cors policy added

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./src/config/corsOptions");
