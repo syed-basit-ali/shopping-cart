ca8385b102b22b8132cc09b3ebeda3121b955e13
syed-basit-ali <basit.shah94@gmail.com>
2023-04-13 13:37:38 +0500

    Feature: logger created

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,4 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const corsOptions = require("./src/config/corsOptions");
+const { logger } = require("./src/middleware/logEvents");
