06a64d019a7b0273c736506964f1a7d5a3d28771
syed-basit-ali <basit.shah94@gmail.com>
2023-04-13 13:49:55 +0500

    Feature: successfully connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,33 @@
 "use strict";
 require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
 const corsOptions = require("./src/config/corsOptions");
 const { logger } = require("./src/middleware/logEvents");
-const errorHandler = require("./middleware/errorHandler");
+const errorHandler = require("./src/middleware/errorHandler");
 const verifyJWT = require("./src/middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require("./src/middleware/credentials");
+const mongoose = require("mongoose");
+const connectDB = require("./src/config/dbConn");
+const PORT = process.env.PORT || 3500;
+
+connectDB();
+
+app.use(logger);
+app.use(credentials);
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
